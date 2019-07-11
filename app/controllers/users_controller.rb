f50de7656edd8add95475051b9178346f88b523c
gbuchanan-snap <gregb@snap-raise.com>
2019-07-11 16:15:10 -0700

    Add users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,22 +4,27 @@ class UsersController < ActionController::API
   end
 
   def index
-    render
+    render json: User.all
   end
 
   def create
-
-  end
-
-  def new
-
-  end
-
-  def edit
-
+    user = User.new(user_params)
+    if user.save
+      render json: user.as_json( except: [:password_digest] )
+    end
   end
 
   def update
+    User.find(params[:id]).update_attributes(update_params)
+  end
 
+  private def user_params
+    params.require(:data).require(:attributes)
+      .permit(:first_name,
+              :last_name,
+              :email,
+              :username,
+              :password,
+              :password_confirmation)
   end
 end
